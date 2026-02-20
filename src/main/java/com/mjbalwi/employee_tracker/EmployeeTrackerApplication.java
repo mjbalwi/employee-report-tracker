998b9554fe838e2536eb0180e54b6223af85aa35
Majeed <mj.balwi@gmail.com>
2026-02-19 23:03:02 -0800

    initial commit

---
+++ src/main/java/com/mjbalwi/employee_tracker/EmployeeTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.mjbalwi.employee_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTrackerApplication.class, args);
+    }
+}
